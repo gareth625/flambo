472cb159af1dffbbe3d7f013940090045d9d66a0
Gareth Rogers <gareth.j.rogers@gmail.com>
2017-12-27 10:51:06 +0000

    Removed accidental commit

--- test/flambo/function_test.clj
+++ test/flambo/function_test.clj
@@ -1,34 +1,3 @@
 (ns flambo.function-test
   (:use midje.sweet)
   (:require [flambo.function :as func]))
-
-(defmacro gen-udfs
-  []
-  (let [arglists# (mapv (fn [i#] (mapv (comp gensym str char) (range 97 (+ 97 i#))))
-                         (range 1))
-        udfs#     (mapv (fn [args#] `(func/udf ~@args#
-                                               (reduce + args#)))
-                        arglists#)]
-    udfs#))
-
-(comment
-
-  (gen-udfs)
-
-
-  (func/udf [x] (inc x))
-
-  )
-
-
-(facts
- "about SparkSQL UDFs"
-
- (fact))
-
-
-(comment
-
-
-
-  )
